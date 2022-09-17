dd4a152aada1684ccd942206e92430e15540309f
msato0731 <rhcq.3127@gmail.com>
2022-09-17 18:05:58 +0900

    add: terraform doc

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,9 @@
 resource "aws_ecr_repository" "this" {
   name = var.name
-
+  force_delete = true
+  image_scanning_configuration {
+    scan_on_push = var.image_scan_on_push
+  }
   tags = {
     Name = var.name
   }
